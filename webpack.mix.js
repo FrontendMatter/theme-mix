4fa311eb37fcf927e8003a0062e7c454584d63f0
Laza Bogdan <laza.bogdan@macro-net.ro>
2017-06-07 14:49:11 +0300

    update configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ let webpackConfig = {}
 ///////////////////
 
 const Config = require('merge-config')
-let config = new Config().merge({
+let config = new Config()
+
+config.merge({
   runTasks: {
     clean: true,
     js: true,
@@ -20,16 +22,36 @@ let config = new Config().merge({
     html: true,
   },
   enableCssThemes: true,
+  // create .rtl.css
+  enableCssRTL: true,
+  // node_modules assets list i.e. 
+  // copy: ['bootstrap/dist/bootstrap.js']
+  // => will get copied to dist/assets/vendor/bootstrap.js
+  copy: [],
+  // options passed to laravel-mix
+  laravelMixOptions: {
+    // ignore fonts
+    processCssUrls: false
+  },
+  browserSync: require('./bs-config.json')
 })
+
+////////////////////////
+// User configuration //
+////////////////////////
 
 try {
   config.file(path.join(process.cwd(), 'theme-mix.yaml'))
-  config.file(path.join(process.cwd(), 'theme-mix.json'))
 }
-catch (e) {}
+catch (e) {
+  if (e.message.indexOf('ENOENT') === -1) {
+    console.error(e.message)
+    process.exit()
+  }
+}
 
-console.log(config.get())
-process.exit()
+// console.log(config.get())
+// process.exit()
 
 ///////////////////////////////////////////
 // RUN SPECIFIC TASKS                    //
@@ -45,7 +67,7 @@ const __RUN = argv.env ? argv.env.run : undefined
 // CLEANUP //
 /////////////
 
-if (__RUN === 'clean' || !__RUN) {
+if (__RUN === 'clean' || (!__RUN && config.get('runTasks:clean'))) {
   del.sync([
     'temp/',
     'dist/**/*.html',
@@ -58,7 +80,7 @@ if (__RUN === 'clean' || !__RUN) {
 ////////
 
 // npm run development -- --env.run js
-if (__RUN === 'js' || !__RUN) {
+if (__RUN === 'js' || (!__RUN && config.get('runTasks:js'))) {
   for (let file of glob.sync('./src/js/**/**.{js,vue}', { ignore: '**/_*' })) {
     mix.js(file, './dist/assets/js')
   }
@@ -70,7 +92,7 @@ if (__RUN === 'js' || !__RUN) {
 ////////////////////////
 
 // npm run development -- --env.run copy
-if (__RUN === 'copy' || !__RUN) {
+if (__RUN === 'copy' || (!__RUN && config.get('runTasks:copy'))) {
   try {
     require(path.join(process.cwd(), 'theme-mix.copy.json')).forEach(function(asset) {
       var dest = path.join(process.cwd(), 'dist/assets/vendor')
@@ -91,73 +113,72 @@ if (__RUN === 'copy' || !__RUN) {
 // SASS //
 //////////
 
+mix.options(config.get('laravelMixOptions'))
+
 // npm run development -- --env.theme dark
 const __THEME = argv.env ? argv.env.theme || 'default' : 'default'
 
 // npm run development -- --env.run sass
-if (__RUN === 'sass' || !__RUN) {
-
+if (__RUN === 'sass' || (!__RUN && config.get('runTasks:sass'))) {
+  let __DIST_CSS = './dist/assets/css'
   let sassOptions = { 
     importer: require('sass-importer-npm'),
-    // inject $theme variable
-    data: '$theme: ' + __THEME + ';'
   }
 
-  mix.options({
-    // ignore fonts
-    processCssUrls: false
-  })
+  if (config.get('enableCssThemes')) {
+    // inject $theme variable
+    sassOptions.data = '$theme: ' + __THEME + ';'
+    __DIST_CSS += '/themes/' + __THEME
+  }
 
   for (let file of glob.sync('src/sass/*.scss', { ignore: '**/_*' })) {
-    mix.sass(file, './dist/assets/css/themes/' + __THEME, sassOptions)
+    mix.sass(file, __DIST_CSS, sassOptions)
   }
-}
 
-/////////
-// RTL //
-/////////
+  /////////
+  // RTL //
+  /////////
 
-// npm run development -- --env.run sass
-if (__RUN === 'sass' || !__RUN) {
-  const WebpackRTLPlugin = require('webpack-rtl-plugin')
-  const WebpackRTLWrapPlugin = require('webpack-rtl-wrap-plugin')
-  const cacheDirectory = path.resolve(path.join('temp', 'rtl', __THEME))
+  if (config.get('enableCssRTL')) {
+    const WebpackRTLPlugin = require('webpack-rtl-plugin')
+    const WebpackRTLWrapPlugin = require('webpack-rtl-wrap-plugin')
+    const cacheDirectory = path.resolve(path.join('temp', 'rtl', __THEME))
 
-  del.sync(cacheDirectory)
+    del.sync(cacheDirectory)
 
-  webpackConfig = merge(webpackConfig, { 
-    plugins: [
-      // Creates .rtl.css
-      new WebpackRTLPlugin({
-        minify: false
-      }),
-      // wraps CSS into [dir=ltr|rtl]
-      new WebpackRTLWrapPlugin({
-        cacheDirectory
-      })
-    ]
-  })
-}
+    webpackConfig = merge(webpackConfig, { 
+      plugins: [
+        // Creates .rtl.css
+        new WebpackRTLPlugin({
+          minify: false
+        }),
+        // wraps CSS into [dir=ltr|rtl]
+        new WebpackRTLWrapPlugin({
+          cacheDirectory
+        })
+      ]
+    })
+  }
 
-///////////////////////////////////
-// WRAP CSS with .theme-$__THEME //
-///////////////////////////////////
+  ///////////////////////////////////
+  // WRAP CSS with .theme-$__THEME //
+  ///////////////////////////////////
 
-// npm run development -- --env.run sass
-if (__RUN === 'sass' || !__RUN) {
-  const WebpackWrapThemePlugin = require('./webpack-wrap-theme-plugin')
-  const cacheDirectory = path.resolve(path.join('temp', 'themeClass', __THEME))
+  if (config.get('enableCssThemes')) {
+    const WebpackWrapThemePlugin = require('./webpack-wrap-theme-plugin')
+    const cacheDirectory = path.resolve(path.join('temp', 'themeClass', __THEME))
 
-  del.sync(cacheDirectory)
+    del.sync(cacheDirectory)
 
-  webpackConfig = merge(webpackConfig, {
-    plugins: [
-      new WebpackWrapThemePlugin({ 
-        themeClass: '.theme-' + __THEME,
-        cacheDirectory
-      })
-    ]
-  })
+    webpackConfig = merge(webpackConfig, {
+      plugins: [
+        new WebpackWrapThemePlugin({ 
+          themeClass: '.theme-' + __THEME,
+          cacheDirectory
+        })
+      ]
+    })
+  }
 }
 
 //////////////
@@ -165,7 +186,7 @@ if (__RUN === 'sass' || !__RUN) {
 //////////////
 
 // npm run development -- --env.run html
-if (__RUN === 'html' || !__RUN) {
+if (__RUN === 'html' || (!__RUN && config.get('runTasks:html'))) {
   for (let file of glob.sync('./src/html/pages/*.html', { ignore: '**/_*' })) {
     Mix.entry().entry.add('mix', path.resolve(file))
   }
@@ -205,5 +226,5 @@ mix.webpackConfig(webpackConfig)
 /////////////////
 
 if (!__RUN) {
-  mix.browserSync(require('./bs-config.json'))
+  mix.browserSync(config.get('browserSync'))
 }
